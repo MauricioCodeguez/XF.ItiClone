bb77e81f36d47abc16af68ce45281792cb63f870
Mauricio Codeguez <mauricio.codeguez@gmail.com>
2020-05-06 01:48:57 -0300

    Criar render para exibir os textos dos botões em letras minusculas

---
+++ ItiClone.Android/Renderers/ButtonAllCapsRenderer.cs
@@ -0,0 +1,23 @@
+﻿using Android.Content;
+using ItiClone.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Button), typeof(ButtonAllCapsRenderer))]
+namespace ItiClone.Droid.Renderers
+{
+    public class ButtonAllCapsRenderer : ButtonRenderer
+    {
+        public ButtonAllCapsRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+                Control.SetAllCaps(false);
+        }
+    }
+}
--- ItiClone.Android/Renderers/CustomTabbedPageRenderer.cs
+++ ItiClone.Android/Renderers/CustomTabbedPageRenderer.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+﻿using Android.Content;
 using ItiClone.Droid.Renderers;
-using PanCardView.Extensions;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 using Xamarin.Forms.Platform.Android.AppCompat;
