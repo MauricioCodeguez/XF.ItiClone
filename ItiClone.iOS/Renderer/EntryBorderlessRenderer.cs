b2777820001d594e3bfd02a36221329c5921477d
Mauricio Codeguez <mauricio.codeguez@gmail.com>
2020-05-12 15:45:49 -0300

    Adicionando renderer para tirar a borda do botão no ios

---
+++ ItiClone.iOS/Renderer/EntryBorderlessRenderer.cs
@@ -0,0 +1,25 @@
+﻿using ItiClone.iOS.Renderer;
+using ItiClone.Renderers;
+using System.ComponentModel;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(EntryBorderless), typeof(EntryBorderlessRenderer))]
+namespace ItiClone.iOS.Renderer
+{
+    public class EntryBorderlessRenderer : EntryRenderer
+    {
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (Control != null)
+            {
+                Control.Layer.BorderWidth = 0;
+                Control.BorderStyle = UITextBorderStyle.None;
+            }
+        }
+    }
+}
